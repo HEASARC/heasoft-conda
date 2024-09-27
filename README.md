ff3fdab166f691de10998a6e37c88cb0991cc9bb
Abdu Zoghbi <a.zoghbi@nasa.gov>
2024-09-27 16:35:38 -0400

    a wokring version for cfitsio tested on linux-64 and osx-arm64

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-- Using rattler-build: https://github.com/prefix-dev/rattler-build/
+
+- If you don't have conda/mamba installed, it is recommended that you install them through [micromamba](https://mamba.readthedocs.io/en/latest/installation/micromamba-installation.html): 
+
+```sh
+"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
+```
+
+- These builds use [rattler-build](https://github.com/prefix-dev/rattler-build/), which can be installed with:
+```sh
+mamba install rattler-build
+```
+ `rattler-build` is similar to `conda-build`, written in rust, which appears to be more robust and easy to use than conda-build.
+
+ - `healib` is a basic hello world example of creating a conda package.
--- cfitsio/build.sh
+++ cfitsio/build.sh
@@ -1,16 +1,7 @@
-#! /bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+#!/bin/bash
 
 set -ex
 
-if [ $(uname) = Darwin ] ; then
-    # Needed to get 'union semun' definition used in drvrsmem.c:
-    export CFLAGS="$CFLAGS -D_DARWIN_C_SOURCE"
-    export LDFLAGS="$LDFLAGS -Wl,-reexport_library,$PREFIX/lib/libz.dylib"
-fi
-#cd cfitsio-4.5.0
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 ./configure --prefix=$PREFIX --with-bzip2 --enable-reentrant || { cat config.log ; exit 1 ; }
 
 make
@@ -21,15 +12,9 @@ if [ ${CONDA_BUILD_CROSS_COMPILATION:-0} -eq 0 ] ; then
     $PREFIX/bin/cookbook
     $PREFIX/bin/speed
     # Actual test suite as described in docs/cfitsio.doc
-    cp testprog $PREFIX/bin/
-    $PREFIX/bin/testprog > testprog.lis
+    ./testprog > testprog.lis
     diff testprog.lis testprog.out
     cmp testprog.fit testprog.std
 fi
 
 rm -f $PREFIX/bin/cookbook $PREFIX/bin/speed $PREFIX/bin/testprog
-
-# check symbol exports on osx
-if [ $(uname) = Darwin ]; then
-    ${OTOOL} -l $PREFIX/lib/libcfitsio.dylib | grep "LC_REEXPORT_DYLIB"
-fi
