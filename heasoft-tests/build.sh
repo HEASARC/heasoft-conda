632f884c697cafa23a8c576a03cd4820208df79b
Abdu Zoghbi <a.zoghbi@nasa.gov>
2024-12-18 10:51:10 -0500

    add heasoft-tests package that containns unnit test comands

---
+++ heasoft-tests/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+ostype=$(uname)
+if [ "$ostype" = "Darwin" ]; then
+    # use headers from libx11 not the ones shiped with tk
+    # with this, xserver works on mac, but not tkpgplot
+    # This can be done at the user end by: mamba install xorg-libx11 --clobber
+    find $PREFIX/include/X11 -name "*.h__clobber-from-xorg-*" \
+        -exec sh -c 'mv "$0" "${0%%__clobber-from-xorg-libx11}"' {} \;
+
+    # remove extra @rpath
+    for conf in `find . -type f -name "configure" -path "*BUILD_DIR*"`; do
+        sed -i '' 's|-Wl,-rpath,\\$HD_TOP_EXEC_PFX/lib||g' $conf
+    done
+
+    # fix python library in mac x86_64
+    hware=$(uname -m)
+    if [ "$hware" = "x86_64" ]; then
+        for conf in `find . -type f -name "configure" -path "*BUILD_DIR*"`; do
+            sed -i '' 's|PYTHON_LIBRARY=`${PY_CONFIG} --ldflags|PYTHON_LIBRARY=`${PY_CONFIG} --libs --embed|g' $conf
+        done
+    fi
+fi
+
+# clean non-utf-8 characters; rattler does not like that in source code
+
+for file in `find ftools/asca suzaku -type f ! -executable -exec file --mime-type {} + \
+        | grep 'text/' | cut -d: -f1`; do
+    iconv -f utf-8 -t utf-8 -c $file -o $file.utf8
+    mv $file.utf8 $file
+done
+
+
+configure_args=(
+    --prefix=$PREFIX
+    --enable-collapse=all
+    --x-includes=$PREFIX/include
+    --x-libraries=$PREFIX/lib
+)
+
+
+cd BUILD_DIR
+./configure "${configure_args[@]}" 2>&1 | tee config.log.txt || false
+#make 2>&1 | tee build.log.txt || false
+#make install 2>&1 | tee install.log.txt || false
+make test || false 
+make install-test || false
+rm -rf $PREFIX/BUILD_DIR/hd_install.o
