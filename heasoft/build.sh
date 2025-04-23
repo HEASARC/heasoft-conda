326777875bb0eedd2c040a0cd217e2b8e0d93ae8
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-04-23 09:54:52 -0400

    backport the init fixes used in 6.35.1 to 6.35

--- heasoft/build.sh
+++ heasoft/build.sh
@@ -41,6 +41,9 @@ if [ "$ostype" = "Darwin" ]; then
     mv $PREFIX/lib/libtk8.6.dylib $PREFIX/lib/libtk8.6.dylib.off
 fi
 
+mv headas-un* BUILD_DIR
+chmod +x BUILD_DIR/headas-unset
+
 cd BUILD_DIR
 ./configure "${configure_args[@]}" 2>&1 | tee config.log.txt || false
 make 2>&1 | tee build.log.txt || false
@@ -83,7 +86,11 @@ chmod +x $PREFIX/bin/heainit.csh
 cat <<EOF >$PREFIX/bin/.heasoft-post-link.sh
 mkdir -p \$CONDA_PREFIX/etc/conda/activate.d
 cp \$CONDA_PREFIX/bin/heainit.*sh \$CONDA_PREFIX/etc/conda/activate.d/
+
+mkdir -p \$CONDA_PREFIX/etc/conda/deactivate.d
+cp \$CONDA_PREFIX/$HEA_SUBDIR/BUILD_DIR/headas-uninit.*sh \$CONDA_PREFIX/etc/conda/deactivate.d/
 EOF
 cat <<EOF >$PREFIX/bin/.heasoft-pre-unlink.sh
 rm \$CONDA_PREFIX/etc/conda/activate.d/heainit.*sh > /dev/null 2>&1
+rm \$CONDA_PREFIX/etc/conda/deactivate.d/headas-uninit.*sh > /dev/null 2>&1
 EOF
